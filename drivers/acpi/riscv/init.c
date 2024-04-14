8fc5f50a698f4f921c27dee6a300fb6b4e539624
Sunil V L <sunilvl@ventanamicro.com>
2024-04-14 19:54:03 +0530

    ACPI: bus: Add acpi_riscv_init function
    Add a new function for RISC-V to do architecture specific initialization
    similar to acpi_arm_init(). Some of the ACPI tables are architecture
    specific and there is no reason trying to find them on other
    architectures.
    
    Signed-off-by: Sunil V L <sunilvl@ventanamicro.com>

---
+++ drivers/acpi/riscv/init.c
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (C) 2023-2024, Ventana Micro Systems Inc
+ *	Author: Sunil V L <sunilvl@ventanamicro.com>
+ *
+ */
+
+#include <linux/acpi.h>
+
+void __init acpi_riscv_init(void)
+{
+}
