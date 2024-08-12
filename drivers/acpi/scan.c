fe161c5837e27affea976fc5c686a20ac6c1ddb3
Sunil V L <sunilvl@ventanamicro.com>
2024-08-12 06:29:17 +0530

    ACPI: scan: Add RISC-V interrupt controllers to honor list
    RISC-V PLIC and APLIC will have dependency from devices using GSI. So, add
    these devices to the honor list.
    
    Signed-off-by: Sunil V L <sunilvl@ventanamicro.com>
    Tested-by: Björn Töpel <bjorn@rivosinc.com>
    Link: https://patch.msgid.link/20240812005929.113499-6-sunilvl@ventanamicro.com
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/scan.c
+++ drivers/acpi/scan.c
@@ -794,6 +794,9 @@ static const char * const acpi_honor_dep_ids[] = {
 	"INTC1059", /* IVSC (TGL) driver must be loaded to allow i2c access to camera sensors */
 	"INTC1095", /* IVSC (ADL) driver must be loaded to allow i2c access to camera sensors */
 	"INTC100A", /* IVSC (RPL) driver must be loaded to allow i2c access to camera sensors */
+	"INTC10CF", /* IVSC (MTL) driver must be loaded to allow i2c access to camera sensors */
+	"RSCV0001", /* RISC-V PLIC */
+	"RSCV0002", /* RISC-V APLIC */
 	NULL
 };
 
